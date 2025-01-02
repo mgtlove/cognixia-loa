6607b8300c676649c3b011d10b794aa9be5f2044
mgtlove <mgtlove@gmail.com>
2025-01-02 16:28:20 -0600

    tracing import issues on login form still, custom button cause package issues, changed to boostrap standard.

--- app/amplify-cognito-config.js
+++ app/amplify-cognito-config.js
@@ -1,47 +1,60 @@
-"use client";
-
-import { Amplify } from 'aws-amplify';
-import {Auth} from 'aws-amplify';
-import React , {createContext, useState, useEffect, useContext} from 'react';
+import { Amplify, Auth } from 'aws-amplify';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 
 Amplify.configure({
   Auth: {
     region: process.env.NEXT_PUBLIC_REGION_ID, // Replace with your Cognito region
     userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID, // Replace with your User Pool ID
-    userPoolWebClientId: process.env. NEXT_PUBLIC_USER_POOL_CLIENT_ID, // Replace with your Web Client ID
+    userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID, // Replace with your Web Client ID
   },
-  
-},
-{ssr: true}
-);
+}, { ssr: true });
 
 // Create a context to store the current user
 const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      checkUser();
-    }, []);
-  
-    async function checkUser() {
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkUser = async () => {
       try {
         const currentUser = await Auth.currentAuthenticatedUser();
         setUser(currentUser);
       } catch (error) {
         setUser(null);
+      } finally {
+        setLoading(false);
       }
-      setLoading(false);
-    }
-  
-    return (
-      <AuthContext.Provider value={{ user, setUser, loading }}>
-        {children}
-      </AuthContext.Provider>
-    );
-  };
-  
-  export const useAuth = () => useContext(AuthContext);
-  
+    };
+
+    checkUser();
+
+    const listener = (data) => {
+      switch (data.payload.event) {
+        case 'signIn':
+          checkUser();
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        default:
+          break;
+      }
+    };
+
+    Auth.Hub.listen('auth', listener);
+
+    return () => {
+      Auth.Hub.remove('auth', listener);
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
