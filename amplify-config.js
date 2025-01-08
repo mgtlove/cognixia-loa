1f7444c6af247edb3badac2fdb13e03ed5a78f97
mgtlove <mgtlove@gmail.com>
2025-01-08 14:35:18 -0600

    issue with amplify installed and sandboxes for outputs, had to update brew.

--- amplify-config.js
+++ amplify-config.js
@@ -2,6 +2,4 @@ import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import outputs from "/amplify_outputs.json";
 
-amplify_outputs.json
-
 Amplify.configure(outputs);
