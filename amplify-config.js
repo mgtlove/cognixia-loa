2734ca69ead2832c51e83a8a12f01904f5ee5a8f
mgtlove <mgtlove@gmail.com>
2025-01-08 12:51:18 -0600

    major react version changes to align with Amplify, React 18 from 19

--- amplify-config.js
+++ amplify-config.js
@@ -1,5 +1,7 @@
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import outputs from "./amplify_outputs.json";
+import outputs from "/amplify_outputs.json";
+
+amplify_outputs.json
 
 Amplify.configure(outputs);
